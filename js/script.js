52632f2d31d4738f00b07c0214af1b6bfa2b6660
Louis Merlin <louis.merlin@epfl.ch>
2015-09-23 17:56:24 +0200

    Working graph

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+initiateTable = function() {
+  document.getElementById("numTable").innerHTML = "<tr><td>T</td><td>X</td><td>V</td><td>A</td></tr>";
+}
+
+var ctx = "";
+initiateGraph = function() {
+  ctx = document.getElementById("graph").getContext("2d");
+  ctx.fillStyle="black";
+  ctx.fillRect(18, 20, 2, 600);
+  ctx.fillRect(18, 620, 602, 2);
+  ctx.fillRect(14, 319, 10, 2);
+}
+
+processData = function(l){
+  createLine(l);
+  plot(l);
+}
+
 createLine = function(l) {
   var str = "<tr>";
   for(var i=0; i<l.length;i++){
@@ -5,4 +23,8 @@ createLine = function(l) {
   }
   str+="</tr>";
   document.getElementById("numTable").innerHTML+=str;
+}
+
+plot = function(l) {
+  ctx.fillRect(l[0]*30 + 20,640 - (l[1]*75 + 320), 2, 2);
 }
--- js/sim.js
+++ js/sim.js
@@ -6,12 +6,16 @@ var m = 1,
     dt = 0.001,
     tf = 20;
 
+
+initiateTable();
+initiateGraph();
 for (t=0; t<=tf; t+=dt) {
   t = +t.toFixed(3);
   var f = -k * x;
   var a = f / m;
   v = v + a * dt;
   x = x + v * dt;
+  plot ([t, x.toFixed(3), v.toFixed(3), a.toFixed(3)]);
   if(t%0.5==0)
     createLine([t, x.toFixed(3), v.toFixed(3), a.toFixed(3)]);
 }
